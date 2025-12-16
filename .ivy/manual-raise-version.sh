a094f39f4375f81f6dee9562ce2af7d4a0e91ef5
Lukas Lieb <lukas.lieb@axonivy.com>
2025-12-16 15:38:17 +0100

    Fix version raise after release

--- .ivy/manual-raise-version.sh
+++ .ivy/manual-raise-version.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+set -e
 
-mvn -f 'pom.test.xml' -B versions:set -DnewVersion=${1} -DprocessAllModules
-mvn -f 'pom.test.xml' -B versions:commit -DprocessAllModules
+mvn -B versions:set -DnewVersion=${1} -DprocessAllModules
+mvn -B versions:commit -DprocessAllModules
+sed -i -E "/<parent>/,/<\/parent>/s/<version>[^<]*<\/version>/<version>${1}<\/version>/" pom.test.xml
+mvn -B -f pom.test.xml versions:set -DnewVersion=${1} -DprocessAllModules
+mvn -B -f pom.test.xml versions:commit -DprocessAllModules
